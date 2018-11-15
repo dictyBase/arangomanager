40069c535c6fe96670ef1c3bacfb9886613fcd73
Siddhartha Basu <biosidd@gmail.com>
2018-11-15 01:04:23 +0000

    feat: add initial go files

---
+++ result.go
@@ -0,0 +1,41 @@
+package arangomanager
+
+import (
+	"context"
+	"fmt"
+
+	driver "github.com/arangodb/go-driver"
+	"github.com/fatih/structs"
+)
+
+// Result is a cursor for single row of data
+type Result struct {
+	cursor driver.Cursor
+	ctx    context.Context
+	empty  bool
+}
+
+// IsEmpty checks for empty result
+func (r *Result) IsEmpty() bool {
+	return r.empty
+}
+
+// Read read the row of data to i interface
+func (r *Result) Read(i interface{}) error {
+	meta, err := r.cursor.ReadDocument(nil, i)
+	if err != nil {
+		return fmt.Errorf("error in reading document %s", err)
+	}
+	if !structs.IsStruct(i) {
+		return nil
+	}
+	s := structs.New(i)
+	if f, ok := s.FieldOk("DocumentMeta"); ok {
+		if f.IsEmbedded() {
+			if err := f.Set(meta); err != nil {
+				return fmt.Errorf("error in assigning DocumentMeta to the structure %s", err)
+			}
+		}
+	}
+	return nil
+}
