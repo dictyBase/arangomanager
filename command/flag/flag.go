310fa13537bec9e7724397223300579d3db512c5
Siddhartha Basu <biosidd@gmail.com>
2022-07-07 06:35:09 -0500

    feat: add required flags for arangodb connection parameters

--- command/flag/flag.go
+++ command/flag/flag.go
@@ -5,29 +5,33 @@ import (
 )
 
 // ArangodbFlags returns the cli based flag slice that includes
-// command line arguments for connecting to an arangodb instance
+// command line arguments for connecting to an arangodb instance.
 func ArangodbFlags() []cli.Flag {
 	return []cli.Flag{
 		cli.StringFlag{
 			Name:   "arangodb-pass, pass",
 			EnvVar: "ARANGODB_PASS",
 			Usage:  "arangodb database password",
+			Required: true,
 		},
 		cli.StringFlag{
 			Name:   "arangodb-database, db",
 			EnvVar: "ARANGODB_DATABASE",
 			Usage:  "arangodb database name",
+			Required: true,
 		},
 		cli.StringFlag{
 			Name:   "arangodb-user, user",
 			EnvVar: "ARANGODB_USER",
 			Usage:  "arangodb database user",
+			Required: true,
 		},
 		cli.StringFlag{
 			Name:   "arangodb-host, host",
 			Value:  "arangodb",
 			EnvVar: "ARANGODB_SERVICE_HOST",
 			Usage:  "arangodb database host",
+			Required: true,
 		},
 		cli.StringFlag{
 			Name:   "arangodb-port",
